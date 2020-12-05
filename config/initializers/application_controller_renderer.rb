082f958624e80e158554a2abb000e12778d70175
愿山聖 <asjstksmr@gmail.com>
2020-12-06 08:25:36 +0900

    [fin]study-rails

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5ad402023d9a31cd2f8505bf3ee5ed836a43b2c27f6d4d03c5e37cba690a4b945cf865cad0e5e549f6b17962de576a0dde3b723fa226c5205b38a074b1ec67fa'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'todolists/new'
+  post '/todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
+  get 'top' => 'homes#top'
 end
